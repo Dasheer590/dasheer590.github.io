f8f26138f8bb93f80721dd3b5cd681b766ff9853
Dasheer590 <breezydanbaraka@gmail.com>
2022-09-30 16:10:26 +0300

    ammendment

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Dasheer Baraka</h1>
+        <h5>Hello I am</h5>
+        <h1>Daniel Baraka</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA/>
         <HeaderSocials/>
